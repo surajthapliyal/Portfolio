41ace988aba49d9cdf69bff80fa3aff385d6e4a2
Suraj <surajthapliyal18@gmail.com>
2021-08-13 11:14:33 +0530

    refresh fixed

--- src/Components/Home/LeftCol.jsx
+++ src/Components/Home/LeftCol.jsx
@@ -5,9 +5,9 @@ import SocialLinks from './../SocialLinks';
 const LeftCol = () => {
   return (
     <div className="leftCol">
-      <p className="name">Hi all, I'm Suraj </p>
+      <p className="name homepagename">Hello World, I'm Suraj </p>
       <p className="para">
-        A passionate Full Stack Software Developer🚀 having an experience of
+        A passionate Full Stack Software Developer having an experience of
         building Web applications with JavaScript / ReactJs / NodeJs / Java
         SpringBoot and some other cool libraries and frameworks.
       </p>
