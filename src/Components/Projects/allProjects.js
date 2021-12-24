dcbbd0cd8f1e5461c6ce3ded1e24d9ad70e64f39
Suraj <surajthapliyal18@gmail.com>
2021-12-24 22:42:34 +0530

    updated

--- src/Components/Projects/allProjects.js
+++ src/Components/Projects/allProjects.js
@@ -1,5 +1,11 @@
 export const allProjects = [
   {
+    name: "Path Finding Visualizer",
+    description:
+      "A web based application to visualize Graph datastructure algorithms such as DFS, BFS, Dijkstra Shortest Path and A star. ",
+    link: "https://github.com/surajthapliyal/Path-Finding-Visualizer/tree/master",
+  },
+  {
     name: "Musically",
     description:
       "A web based application where users can search, play, pause, stop and resume any song of their preference. Created using ReactJS, Css and Axios",
@@ -8,7 +14,7 @@ export const allProjects = [
   {
     name: "CHATS",
     description:
-      "A ReactJS, NodeJS based application where user can ,chat and share images to each other.",
+      "A ReactJS, NodeJS based application where user can chat and share images to each other.",
     link: "https://github.com/surajthapliyal/Chats/tree/master",
   },
   {
